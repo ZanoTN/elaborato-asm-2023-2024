e5fd1b807ff418bb70764b79ba7159d3833037d0
Riccardo Zanotelli <riccardo.zanotelli@gmail.com>
2024-07-10 20:28:39 +0200

    Refactor to one algorithm

--- C/main.c
+++ C/main.c
@@ -11,8 +11,7 @@ typedef struct prodotto
 } prodotto;
 
 int menu();
-void EDF(prodotto *arr, int len);
-void HPF(prodotto *arr, int len);
+void algorith(prodotto *arr, int len, int type);
 int penalita(prodotto prodotto, int tempo_inizio);
 
 int main(int argc, char *argv[])
@@ -52,119 +51,63 @@ int main(int argc, char *argv[])
     algoritmo = menu();
 
     // Usiamo l'agoritmo scelto dall'utente
-    if (algoritmo == 1)
+    if (algoritmo == 1 || algoritmo == 2)
     {
-      EDF(prodotti, i);
+      algorith(prodotti, i, algoritmo);
     }
-    else if (algoritmo == 2)
-    {
-      HPF(prodotti, i);
-    }
+
   } while (algoritmo != -1);
 }
 
 int menu()
 {
-  int input_utente;
-
-  printf("\nSelezione un tipo di algoritmo: (-1 per uscire) ");
-
+  int input_utente = 0;
+  printf("\nSelezione un tipo di algoritmo: (-1 per uscire): ");
   scanf("%d", &input_utente);
   return input_utente;
 }
 
-// Controllo per scandeza e priorità
-void EDF(prodotto *arr, int len)
+/** Lancia il comando di ordinamento. 1=EDF 2=HPF */
+void algorith(prodotto *arr, int len, int type)
 {
   int scambia = 0;
+  char *nomeAlgoritmo = type == 1 ? "EDF" : "HPF";
   prodotto temp;
+
   // Ordiniamo per scandenza
   for (int i = 0; i < len; i++)
   {
     prodotto prodotto_minore = arr[i];
     for (int i_min = i; i_min < len; i_min++)
     {
-      if (arr[i_min].scadenza <= prodotto_minore.scadenza)
-      {
-        scambia = 0;
-        if (arr[i_min].scadenza == prodotto_minore.scadenza)
-        {
-          if (arr[i_min].priorita > prodotto_minore.priorita)
-          {
-            scambia = 1;
-          }
-        }
-        else
-        {
-          scambia = 1;
-        }
+      scambia = 0;
+      if (
+          (type == 1 && arr[i_min].scadenza < prodotto_minore.scadenza) ||
+          (type == 2 && arr[i_min].priorita > prodotto_minore.priorita)
+      ) {
+        scambia = 1;
+      }
 
-        if (scambia == 1)
-        {
-          temp = arr[i];
-          arr[i] = arr[i_min];
-          arr[i_min] = temp;
-        }
+      if(type == 1 && arr[i_min].scadenza == prodotto_minore.scadenza && arr[i_min].priorita > prodotto_minore.priorita) {
+        scambia = 1;
+      }
+
+      if(type == 2 && arr[i_min].priorita == prodotto_minore.priorita && arr[i_min].scadenza < prodotto_minore.scadenza) {
+        scambia = 1;
+      }
+
+      if (scambia == 1)
+      {
+        temp = arr[i];
+        arr[i] = arr[i_min];
+        arr[i_min] = temp;
       }
     }
   }
 
   int fine_ultimo_prodotto = 0;
   int totale_penalita = 0;
-  printf("Pianificazione EDF:\n");
-  for (int i = 0; i < len; i++)
-  {
-    printf("%d:%d\n", arr[i].ID, fine_ultimo_prodotto);
-    totale_penalita += penalita(arr[i], fine_ultimo_prodotto);
-    fine_ultimo_prodotto += arr[i].durata;
-  }
-
-  // Conculusione
-  printf("Conclusine: %d\n", fine_ultimo_prodotto);
-
-  // Penalità
-  printf("Penalty: %d\n", totale_penalita);
-}
-
-// Controllo per priorità e scadenza
-void HPF(prodotto *arr, int len)
-{
-  int scambia = 0;
-  prodotto temp;
-  // Ordiniamo per scandenza
-  for (int i = 0; i < len; i++)
-  {
-    prodotto prodotto_minore = arr[i];
-    for (int i_min = i; i_min < len; i_min++)
-    {
-      if (arr[i_min].priorita >= prodotto_minore.priorita)
-      {
-        scambia = 0;
-        if (arr[i_min].priorita == prodotto_minore.priorita)
-        {
-          if (arr[i_min].scadenza < prodotto_minore.scadenza)
-          {
-            scambia = 1;
-          }
-        }
-        else
-        {
-          scambia = 1;
-        }
-
-        if (scambia == 1)
-        {
-          temp = arr[i];
-          arr[i] = arr[i_min];
-          arr[i_min] = temp;
-        }
-      }
-    }
-  }
-
-  int fine_ultimo_prodotto = 0;
-  int totale_penalita = 0;
-  printf("Pianificazione HPF:\n");
+  printf("Pianificazione %s:\n", nomeAlgoritmo);
   for (int i = 0; i < len; i++)
   {
     printf("%d:%d\n", arr[i].ID, fine_ultimo_prodotto);
